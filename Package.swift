552fbbe1581864a598e1a597ba9d28cbea7bfce9
Dave Abrahams <dabrahams@adobe.com>
2023-05-17 12:49:38 -0700

    Basics

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TestGeneration",
+    products: [
+        .library(
+            name: "TestGeneration",
+            targets: ["TestGeneration"]),
+    ],
+    targets: [
+        .target(
+            name: "TestGeneration"),
+        .testTarget(
+            name: "TestGenerationTests",
+            dependencies: ["TestGeneration", "TestGeneratorPlugin"]),
+
+    .plugin(
+      name: "TestGeneratorPlugin", capability: .buildTool(),
+      dependencies: [.target(name: "GenerateTests")]),
+
+    .executableTarget(
+      name: "GenerateTests",
+      dependencies: []),
+
+    ]
+)
---
+++ Plugins/TestGeneratorPlugin/TestGeneratorPlugin.swift
@@ -0,0 +1,21 @@
+import PackagePlugin
+
+@main
+struct TestGeneratorPlugin: BuildToolPlugin {
+
+  func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+    guard let target = target as? SourceModuleTarget else { return [] }
+    let inputPaths = target.sourceFiles(withSuffix: "testgen").map(\.path)
+    let outputPath = context.pluginWorkDirectory.appending("GeneratedTests.swift")
+
+    let cmd: Command = .buildCommand(
+        displayName: "Generating XCTestCases for \(inputPaths.map(\.stem)) into \(outputPath)",
+        executable: try context.tool(named: "GenerateTests").path,
+        arguments: inputPaths + [ "-o", outputPath ],
+        inputFiles: inputPaths,
+        outputFiles: [ outputPath ]
+    )
+    return [cmd]
+  }
+
+}
