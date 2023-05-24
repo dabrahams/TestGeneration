965602a8ab6cc3e3348c62e97b2b4d64a95b349f
Dave Abrahams <dabrahams@adobe.com>
2023-05-23 20:52:18 -0700

    More minimization

--- Tests/TestGenerationTests/TestGenerationTests.swift
+++ Tests/TestGenerationTests/TestGenerationTests.swift
@@ -1,11 +1,1 @@
-import XCTest
 
-final class TestGenerationTests: XCTestCase {
-  func testExample() throws {
-    // XCTest Documenation
-    // https://developer.apple.com/documentation/xctest
-
-    // Defining Test Cases and Test Methods
-    // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-  }
-}
