184a44f6e8a0351f54fdf8ec4a65c33bc5920371
Venkata <vsramakrishnaraju@gmail.com>
2023-05-24 00:49:46 -0700

    Command Line Argument Validation: Right now, the script assumes that it has been called with the correct arguments. Adding some validation logic to ensure that the command line arguments are present and valid (i.e., point to actual files) would make the script more robust and user-friendly.
    File Content Analysis: Currently, the script generates test cases based solely on the input file names. Depending on the use case, you might want to analyze the content of those files and generate test cases accordingly.
    
    Test Case Diversity: All generated test cases are the same, asserting that true equals true. To make the generated test cases more valuable, you could add variability in the assertions or even include custom assertions based on the filenames or file content.
    
    Error Handling: Right now, if an error occurs, it will terminate the program. You might want to add a catch block to handle the errors and possibly continue the execution for the remaining files if one fails.
    
    Code Readability: Extract functionality into smaller, well-named functions. This will improve the readability and maintainability of your code.

--- Sources/GenerateTests/GenerateTests.swift
+++ Sources/GenerateTests/GenerateTests.swift
@@ -2,37 +2,50 @@ import Foundation
 
 @main
 struct GenerateTests {
-  static func main() throws {
+    static func main() {
+        do {
+            guard CommandLine.argc > 2 else {
+                throw NSError(domain: "com.example.GenerateTests", code: 1, userInfo: [NSLocalizedDescriptionKey: "Input and output file paths are required"])
+            }
+            
+            let inputs = CommandLine.arguments.dropFirst().dropLast().map { URL(fileURLWithPath: $0) }
+            let output = URL(fileURLWithPath: CommandLine.arguments.last!)
 
-    let inputs = CommandLine.arguments.dropFirst().dropLast()
-      .lazy.map(URL.init(fileURLWithPath:))
+            let generatedSource = try generateTestSource(from: inputs)
+            try writeTestSource(generatedSource, to: output)
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    static func generateTestSource(from files: [URL]) throws -> String {
+        var source =
+        """
+        import XCTest
 
-    let output = URL(fileURLWithPath: CommandLine.arguments.last!)
-
-    var generatedSource =
-      """
-      import XCTest
-
-      final class GeneratedTests: XCTestCase {
-
-      """
-
-    for f in inputs {
-      let testName = f.deletingPathExtension().lastPathComponent
-
-      generatedSource += """
-
-          func test_\(testName)() throws {
-            XCTAssert(true, \(String(reflecting: testName)))
-          }
+        final class GeneratedTests: XCTestCase {
 
         """
+        
+        for file in files {
+            let testName = file.deletingPathExtension().lastPathComponent
+            source += """
+
+                func test_\(testName)() throws {
+                    XCTAssert(true, "\(testName)")
+                }
+
+            """
+        }
+        
+        source += """
+        }
+        """
+        
+        return source
     }
-
-    generatedSource += """
-      }
-      """
-
-    try generatedSource.write(to: output, atomically: true, encoding: .utf8)
-  }
+    
+    static func writeTestSource(_ source: String, to url: URL) throws {
+        try source.write(to: url, atomically: true, encoding: .utf8)
+    }
 }
