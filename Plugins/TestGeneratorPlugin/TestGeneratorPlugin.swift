45673b9d204464f23172b4267b9c2228d12efb75
Dave Abrahams <dabrahams@adobe.com>
2023-08-24 13:42:13 -0700

    Try to work around Windows Path problem

--- Plugins/TestGeneratorPlugin/TestGeneratorPlugin.swift
+++ Plugins/TestGeneratorPlugin/TestGeneratorPlugin.swift
@@ -1,12 +1,15 @@
 import PackagePlugin
+#if os(Windows)
+import WinSDK
+#endif
 
 @main
 struct TestGeneratorPlugin: BuildToolPlugin {
 
   func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
     guard let target = target as? SourceModuleTarget else { return [] }
-    let inputPaths = target.sourceFiles(withSuffix: "testgen").map(\.path)
-    let outputPath = context.pluginWorkDirectory.appending("GeneratedTests.swift")
+    let inputPaths = target.sourceFiles(withSuffix: "testgen").map(\.path.fixedForWindows)
+    let outputPath = context.pluginWorkDirectory.appending("GeneratedTests.swift").fixedForWindows
 
     let cmd: Command = .buildCommand(
         displayName: "Generating XCTestCases for \(inputPaths.map(\.stem)) into \(outputPath)",
@@ -18,4 +21,20 @@ struct TestGeneratorPlugin: BuildToolPlugin {
     return [cmd]
   }
 
+}
+
+extension Path {
+  var fixedForWindows: Path {
+    #if os(Windows)
+    return string.withCString(encodedAs: UTF16.self) { pwszPath in
+      let dwLength = GetFullPathNameW(pwszPath, 0, nil, nil)
+      withUnsafeTemporaryAllocation(of: WCHAR.self, capacity: Int(dwLength)) {
+        GetFullPathNameW(pwszPath, $0.count, $0.baseAddress, nil)
+        return String(utf16CodeUnits: $0, count: $0.count)
+      }
+    }
+    #else
+    return self
+    #endif
+  }
 }
