acae225202c34ed40f997eff9cba62ce3e2386b6
Dave Abrahams <dabrahams@adobe.com>
2023-09-24 15:19:59 -0700

    Minimize the example

--- Plugins/ResourceGeneratorPlugin/ResourceGeneratorPlugin.swift
+++ Plugins/ResourceGeneratorPlugin/ResourceGeneratorPlugin.swift
@@ -4,32 +4,29 @@ import PackagePlugin
 @main
 struct ResourceGeneratorPlugin: PortableBuildToolPlugin {
 
-  func portableBuildCommands(context: PackagePlugin.PluginContext, target: PackagePlugin.Target) throws -> [PortableBuildCommand] {
-    // Rather than trust operations on `PackagePlugin.Path`, we
-    // immediately convert them to `URL`s and do all manipulations
-    // through `URL`'s API, converting back to `Path`s at the last
-    // possible moment.
+  func portableBuildCommands(
+    context: PackagePlugin.PluginContext, target: PackagePlugin.Target
+  ) throws -> [PortableBuildCommand] {
+
     let inputs = (target as! SourceModuleTarget)
-      .sourceFiles(withSuffix: ".in").map(\.path.url)
+      .sourceFiles(withSuffix: ".in").map(\.path)
 
     if inputs.isEmpty { return [] }
 
-    let workDirectory = context.pluginWorkDirectory.url
-    let outputDirectory = workDirectory.appendingPathComponent("GeneratedResources")
+    let workDirectory = context.pluginWorkDirectory
+    let outputDirectory = workDirectory.appending(subpath: "GeneratedResources")
 
     let outputs = inputs.map {
-      outputDirectory.appendingPathComponent(
-        $0.deletingPathExtension().appendingPathExtension("out").lastPathComponent
-      )
+      outputDirectory.appending(subpath: $0.lastComponent.dropLast(2) + "out")
     }
 
     return [
       .buildCommand(
         displayName: "Running converter",
         tool: .executableProduct(name: "GenerateResource"),
-        arguments: inputs.map(\.path) + [ outputDirectory.path ],
-        inputFiles: inputs.map(\.spmPath),
-        outputFiles: outputs.map(\.spmPath))
+        arguments: (inputs + [ outputDirectory ]).map(\.repaired.string),
+        inputFiles: inputs,
+        outputFiles: outputs)
     ]
   }
 
