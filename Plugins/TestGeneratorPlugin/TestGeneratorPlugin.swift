042201d359d836b63087bd8684526b00e5775866
Dave Abrahams <dabrahams@adobe.com>
2026-05-11 19:57:46 -0700

    Update to latest Swift

--- Plugins/TestGeneratorPlugin/TestGeneratorPlugin.swift
+++ Plugins/TestGeneratorPlugin/TestGeneratorPlugin.swift
@@ -1,17 +1,27 @@
 import PackagePlugin
+import Foundation
+
+extension URL {
+
+  /// The representation used by the native filesystem.
+  public var platformString: String {
+    self.withUnsafeFileSystemRepresentation { String(cString: $0!) }
+  }
+
+}
 
 @main
 struct TestGeneratorPlugin: BuildToolPlugin {
 
   func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
     guard let target = target as? SourceModuleTarget else { return [] }
-    let inputPaths = target.sourceFiles(withSuffix: "testgen").map(\.path)
-    let outputPath = context.pluginWorkDirectory.appending("GeneratedTests.swift")
+    let inputPaths = target.sourceFiles(withSuffix: "testgen").map(\.url)
+    let outputPath = context.pluginWorkDirectoryURL.appendingPathComponent("GeneratedTests.swift")
 
     let cmd: Command = .buildCommand(
-        displayName: "Generating XCTestCases for \(inputPaths.map(\.stem)) into \(outputPath)",
-        executable: try context.tool(named: "GenerateTests").path,
-        arguments: inputPaths + [ outputPath ],
+        displayName: "Generating XCTestCases for \(inputPaths) into \(outputPath)",
+        executable: try context.tool(named: "GenerateTests").url,
+        arguments: (inputPaths + [ outputPath ]).map(\.platformString),
         inputFiles: inputPaths,
         outputFiles: [ outputPath ]
     )
